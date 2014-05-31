6bbd26bd2c4589156eb7d0b9c6b13d298b8c5708
challyed <ed.chally@gmail.com>
2014-05-31 15:50:33 -0400

    Update Taskone.rb

--- Taskone.rb
+++ Taskone.rb
@@ -1,3 +1,9 @@
-print('what is your name:')
-name=gets()
-puts("Hello #{name}")
+#a list of put statements
+puts "Hello World"
+puts "Hello again"
+puts "I like typing this"
+puts "This is fun"
+puts "Yea! Printing"
+puts "I rather 'Not'"
+puts 'I "said" do not touch this.'
+
