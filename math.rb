84ae91406e8358f52c2aaf78eb996010dffa3a0d
challyed <ed.chally@gmail.com>
2014-05-31 15:54:05 -0400

    Create math.rb

---
+++ math.rb
@@ -0,0 +1,29 @@
+puts "I will now count my chickens"
+
+puts "Hens", 25 + 30.0/6.0
+#This divides 30 and 6 leaving 5 and adding 5 to 25
+puts "Roosters", 100 - 25 * 3 % 4
+#This is 100 minus 25 times 3 Mod 4. 25*3=75. 75 Mod 4. 100-3
+
+puts "Now I will count my eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4.0 + 6
+#PEDMAS
+puts "is it ture that 3+2<5-7?"
+#does 3+2 and then 5-7 and then compares the answers giving T/F in the end
+puts 3 + 2 < 5 - 7
+#does 3+2 and then 5-7 and then compares the answers
+puts "What is 3+2?", 3 + 2
+#gives the answer to the question -5
+puts "What is 5-7", 5 - 7
+#gives the answer to the question -2
+puts "Oh, that's why it is false"
+
+puts "How about some more"
+
+puts "What is greater?", 5 > -2
+# 5 is greater then -2
+puts "Is it greater or equal?", 5 >= -2
+# 5 is greater then -2
+puts "Is it less or equal?",  5 <= -2
+## 5 is not less then -2
