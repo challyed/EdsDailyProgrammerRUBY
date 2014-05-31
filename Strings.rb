5ec3414975653e2de4b1507831e3f9a8202dd221
challyed <ed.chally@gmail.com>
2014-05-31 16:23:58 -0400

    Create Strings.rb

---
+++ Strings.rb
@@ -0,0 +1,21 @@
+# Here's some new strange stuff, remember to type it in exactly.
+#you cant have a space after paragraph
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+#string of the days
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#string of the months with new lines
+
+puts "Here are the days: ", days
+#puts in the string after the sentence for days
+puts "Here are the months: ", months
+#puts in the string after the sentence for months
+
+
+puts <<PARAGRAPH
+There's something going on here.
+With the PARAGRAPH thing.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAPH
+#paragraph do not use spaces. 
