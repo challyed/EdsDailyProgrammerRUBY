f3cf15b3aba6eb2aa3bc5afaa404adbb7db546e0
challyed <ed.chally@gmail.com>
2014-05-31 15:53:22 -0400

    Update and rename Taskone.rb to Comments.rb

---
+++ Comments.rb
@@ -0,0 +1,16 @@
+#Comments
+#A comment that is so you can read your program later.
+#Anything after # is irgoned by ruby
+puts "I could have code like this." #and the comment after is irgoned
+# you can also also use a comment to disable or comment out a peice of code.
+# Puts "This will not run"
+puts "This will run"
+puts "Hello World"
+puts "Hello again"
+puts "I like typing this"
+puts "This is fun"
+puts "Yea! Printing"
+puts "I rather 'Not'"
+puts 'I "said" do not touch this.'
+
+
--- Taskone.rb
+++
@@ -1,9 +0,0 @@
-#a list of put statements
-puts "Hello World"
-puts "Hello again"
-puts "I like typing this"
-puts "This is fun"
-puts "Yea! Printing"
-puts "I rather 'Not'"
-puts 'I "said" do not touch this.'
-
