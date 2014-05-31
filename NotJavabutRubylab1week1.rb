924f27421aa4c352528980455b6479c78b5f0fd0
challyed <ed.chally@gmail.com>
2014-05-31 16:30:51 -0400

    Create NotJavabutRubylab1week1.rb

---
+++ NotJavabutRubylab1week1.rb
@@ -0,0 +1,13 @@
+shoe = 49.99
+qty = 5
+taxrate = 0.08
+taxamount = 0.00
+subtotal = qty*shoe
+taxamount = subtotal*taxrate
+total =subtotal+taxamount
+
+puts "Product Desc Footwear"
+puts "Quanity #{qty}"
+puts "Price #{shoe}"
+puts "subtotal #{subtotal}"
+puts "Sales Tax #{taxamount}"
