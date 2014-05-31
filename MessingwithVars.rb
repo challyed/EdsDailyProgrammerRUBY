a0206cf0cdcb269d3e1c711a0917d2e0b77cd26a
challyed <ed.chally@gmail.com>
2014-05-31 16:37:00 -0400

    Create MessingwithVars.rb

---
+++ MessingwithVars.rb
@@ -0,0 +1,11 @@
+first, second, third = ARGV 
+#set the argument variable to the 1st 3 variables.
+
+puts "The script is called: #{$0}"
+#Here we see what the script is called.
+puts "Your first variable is: #{first}"
+#pulls the first variable.
+puts "Your second variable is: #{second}"
+#pulls the second variable.
+puts "Your third variable is: #{third}"
+#pulls the thrid variable.
