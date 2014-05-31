b9157f4dbbad001c974514c8ffcf2b648fe9ca47
challyed <ed.chally@gmail.com>
2014-05-31 16:25:43 -0400

    Create askingandgetting.rb

---
+++ askingandgetting.rb
@@ -0,0 +1,34 @@
+print "How old are you?"
+#ask a question
+age= gets.chomp()
+#when user type the answer it gets put into a string.
+print "How tall are you?"
+#ask a question
+height= gets.chomp()
+#when user type the answer it gets put into a string.
+print "How much do you weigh?"
+#ask a question
+weight= gets.chomp()
+#when user type the answer it gets put into a string.
+print "What is your name?"
+#ask a question
+name= gets.chomp()
+#when user type the answer it gets put into a string.
+print "What is your favorite color?"
+#ask a question
+color= gets.chomp()
+#when user type the answer it gets put into a string.
+print "Where do you live?"
+#ask a question
+location= gets.chomp()
+#when user type the answer it gets put into a string.
+
+puts "So, you're #{age} years old, #{height} feet tall, and weigh #{weight}."
+#puts answers in the sentence
+puts "Your name is #{name}, you like #{color}, and you live in #{location}"
+#puts answers in the sentence
+#put get instead of gets
+#you can use gets.chomp for names and numbers
+#chomp removes new lines and gets add them
+#gets 'accepts a single line of data from the standard input'
+#Chomps is a string method  
